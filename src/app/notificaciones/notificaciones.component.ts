f7d54c2e12bcef0967ed2391210b2f923f6db709
FrancoVanney <francovanney83@gmail.com>
2021-03-24 21:20:38 -0300

    Notificaciones leidas

--- src/app/notificaciones/notificaciones.component.ts
+++ src/app/notificaciones/notificaciones.component.ts
@@ -11,6 +11,10 @@ export class NotificacionesComponent implements OnInit {
 
   ngOnInit(): void {
   }
+
+  actualizarVista(notificacion){
+    notificacion.fueLeida = true;
+  }
 }
 
 export interface notificacion{
@@ -18,11 +22,12 @@ export interface notificacion{
   nombre: string;
   cabezaGanado: string;
   alerta: string;
+  fueLeida: boolean;
 }
 
-export const NOTI: notificacion[] = [
-  {id:1, nombre:"Cambio de temperatura", cabezaGanado:"CG142379", alerta:"La temperatura cayo a 35º"},
-  {id:2, nombre:"Falta de movilidad", cabezaGanado:"CG6854864",alerta:"El individuo se ha movilizado 1500 metros menos que el estandar"},
-  {id:3, nombre:"Cambio de temperatura", cabezaGanado:"CG41239847", alerta:"La temperatura subio a 39º"}
+export var NOTI: notificacion[] = [
+  {id:1, nombre:"Cambio de temperatura", cabezaGanado:"CG142379", alerta:"La temperatura cayo a 35º", fueLeida: false},
+  {id:2, nombre:"Falta de movilidad", cabezaGanado:"CG6854864",alerta:"El individuo se ha movilizado 1500 metros menos que el estandar", fueLeida: false},
+  {id:3, nombre:"Cambio de temperatura", cabezaGanado:"CG41239847", alerta:"La temperatura subio a 39º", fueLeida: true}
 ];
 
